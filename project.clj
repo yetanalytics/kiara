081734eca3b0c1160b8dd221dda9bd615ff5d423
Dan Fornika <dan@fornikascience.com>
2016-10-18 12:04:11 -0700

    Bumped version numbers of dependencies

--- project.clj
+++ project.clj
@@ -3,10 +3,10 @@
   :url "http://github.com/quoll"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojars.quoll/turtle "0.2.1"]
-                 [com.datomic/datomic-free "0.9.4556"]
-                 [prismatic/schema "0.2.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojars.quoll/turtle "0.2.2"]
+                 [com.datomic/datomic-free "0.9.5394"]
+                 [prismatic/schema "1.1.3"]]
   :main kiara.test-load
   
   :profiles {:test {:jvm-opts ["-Xms96m" "-Xmx1g" "-Ddatomic.objectCacheMax=128M" "-Ddatomic.memoryIndexMax=64M"]}}
